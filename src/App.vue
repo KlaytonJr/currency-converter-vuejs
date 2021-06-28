9a9b396114d26655e5e4418d2056dce83e64333d
Klayton Junior <klaytonjunior@protonmail.com>
2021-06-28 10:52:25 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Conversor de Moedas</h1>
+    <div class="linha">
+      <Conversor moeda-a="USD" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="USD"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda-a="CAD" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="CAD"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda-a="EUR" moeda-b="BRL"/>
+      <Conversor moeda-a="BRL" moeda-b="EUR"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Conversor from "./components/Conversor.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Conversor
   }
 }
 </script>
 
 <style>
 #app {
+  margin: auto;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 900px;
+}
+
+.linha {
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
